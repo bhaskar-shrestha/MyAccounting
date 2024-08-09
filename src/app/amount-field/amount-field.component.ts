aa90be2e4c95fd04da69717ad9a003183bc66830
Bhas <bhaskar.shrestha@outlook.com>
2024-08-09 13:32:39 +1000

    add base nav and amount components

---
+++ src/app/amount-field/amount-field.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-amount-field',
+  standalone: true,
+  imports: [],
+  templateUrl: './amount-field.component.html',
+  styleUrl: './amount-field.component.scss'
+})
+export class AmountFieldComponent {
+
+}
